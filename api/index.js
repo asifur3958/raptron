f5ea1b2e77028a522aa2b6ba850687021826d2f2
Md. Asifur Rahman <108418125+asifur3958@users.noreply.github.com>
2026-05-05 23:47:49 +0600

    Remove conflicting Vercel config

--- api/index.js
+++
@@ -1,9 +0,0 @@
-import server from "../dist/server/server.js";
-
-export const config = {
-  runtime: "edge",
-};
-
-export default function (request, event) {
-  return server.fetch(request, event);
-}
